381f896ae9e508204484d4324067ab3f475c8940
Ahnsj828 <ala828@naver.com>
2024-07-17 18:29:53 +0900

    study_useState

--- starcoding/src/App.js
+++ starcoding/src/App.js
@@ -1,30 +1,72 @@
-// < useState >
+//  // < useState >
+
+// import { useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [time, setTime] = useState(1);
+
+//   const clikBtn = () => {
+//     setTime((prevTime) => (prevTime % 24) + 1);
+//   };
+
+//   const formatTime = (time) => {
+//     if (time < 12) {
+//       return `오전${time}시`;
+//     } else if (time === 12) {
+//       return `오후${time}시`;
+//     } else if (time === 24) {
+//       return `오전12시`;
+//     } else {
+//       return `오후 ${time - 12}시`;
+//     }
+//   };
+//   return (
+//     <div>
+//       <span>현재시각 {formatTime(time)}</span>
+//       <button onClick={clikBtn}>Update</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//============================================================================
 
 import { useState } from "react";
 import "./App.css";
 
+const heavyWork = () => {
+  console.log("엄청 무거운 작업!!");
+  return ["홍길동", "이영희"];
+};
+
 function App() {
-  const [time, setTime] = useState(1);
+  // const [names, setNames] = useState(["이영희", "홍길동"]);
+  const [names, setNames] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState("");
 
-  const clikBtn = () => {
-    setTime((prevTime) => (prevTime % 24) + 1);
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleUpload = () => {
+    setNames((prevState) => {
+      console.log("이전 state: ", prevState);
+      return [input, ...prevState];
+    });
   };
 
-  const formatTime = (time) => {
-    if (time < 12) {
-      return `오전${time}시`;
-    } else if (time === 12) {
-      return `오후${time}시`;
-    } else if (time === 24) {
-      return `오전12시`;
-    } else {
-      return `오후 ${time - 12}시`;
-    }
-  };
+  // console.log(input);
+
   return (
     <div>
-      <span>현재시각 {formatTime(time)}</span>
-      <button onClick={clikBtn}>Update</button>
+      <input value={input} onChange={handleInputChange} type="text" />
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
     </div>
   );
 }
