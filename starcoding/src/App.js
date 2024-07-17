a96ec58d6e975d890f6439dbdca74b269d6599e4
Ahnsj828 <ala828@naver.com>
2024-07-17 18:30:16 +0900

    study_useState

--- starcoding/src/App.js
+++ starcoding/src/App.js
@@ -72,3 +72,21 @@ function App() {
 }
 
 export default App;
+
+//============================================================================
+
+// < useState >
+// const[state, setState] = useState(초기값)
+
+// 1. setState를 활용해서 state를 변경
+//     state를 변경할 때 마다 컴포넌트는 다시 렌더링이 된다
+
+// 2. state를 변경할 때 새로 변경될 state값이 이전 state 값과 연관이 되어 있다면 setState의 인자로 새로운 state를 리턴하는 콜백 함수를 넣어주는게 좋다
+// setState((prevState) => {
+//   return newState
+// })
+
+// 3. useState를 사용해서 초기 값을 받아올 때 어떤 무거운 일을 해야 된다면 useState의 인자로 콜백 함수를 넣어주면 맨 처음 엔더링이 될 때만 실행되게 할 수 있다.
+// useState(() => {
+//   return heavyWorks()
+// })
