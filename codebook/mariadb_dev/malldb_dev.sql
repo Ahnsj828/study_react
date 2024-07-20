7134628e6b89e0d67e83bf5f01b621ddcafc9abc
Ahnsj828 <ala828@naver.com>
2024-07-20 19:24:20 +0900

    코드로 배우는 리액트 - DB

--- codebook/mariadb_dev/malldb_dev.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE malldb;
-
-
-
-CREATE USER 'malldbuser'@'localhost' IDENTIFIED BY 'malldbuser';
-CREATE USER 'malldbuser'@'%' IDENTIFIED  BY 'malldbuser';
-
-GRANT ALL PRIVILEGES ON malldb.* TO 'malldbuser'@'localhost';
-GRANT ALL PRIVILEGES ON malldb.* TO 'malldbuser'@'%';
