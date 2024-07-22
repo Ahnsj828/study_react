a2773abde174067347bf250b8ab8b8701832c95f
Ahnsj828 <ala828@naver.com>
2024-07-23 00:20:16 +0900

    react-router 모듈 추가

--- codebook/practice/src/App.js
+++ codebook/practice/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold ">Hello World!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
 }
 
 export default App;
