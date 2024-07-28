3846f7ccb0c0bf0bc9589cd5c71f0b6a593b7a4f
Ahnsj828 <ala828@naver.com>
2024-07-28 17:03:12 +0900

    lifecoding study setting

--- lifecoding/src/App.js
+++ lifecoding/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        {/* home으로 이동하는 header영역 */}
+        <h1>
+          <a href="/">WEB</a>
+        </h1>
       </header>
+      <nav>
+        {/* html, css, js와 같은 구체적인 글을 보는 페이지로 이동하는 영역 */}
+        <ol>
+          <li>
+            <a href="/read/1">html</a>
+          </li>
+          <li>
+            <a href="/read/2">css</a>
+          </li>
+          <li>
+            <a href="/read/3">js</a>
+          </li>
+        </ol>
+      </nav>
+      <article>
+        {/* 본문을 표시하는 영역 */}
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
   );
 }
