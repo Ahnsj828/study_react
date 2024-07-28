ca838d937becb9bef45fee8bf8520286ea7db6f0
Ahnsj828 <ala828@naver.com>
2024-07-28 17:20:30 +0900

    사용자 정의 태그 정의 & 사용

--- lifecoding/src/App.js
+++ lifecoding/src/App.js
@@ -1,15 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function Header() {
+  // 사용자 정의 태그를 만들고 싶다면 함수를 정의하면 된다.
+  // 사용자 정의태그는 대문자로 시작해야한다
+  return (
+    // 이 함수는 return값으로 html 코드를 return하면 된다
+    <header>
+      {/* home으로 이동하는 header영역 */}
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        {/* home으로 이동하는 header영역 */}
-        <h1>
-          <a href="/">WEB</a>
-        </h1>
-      </header>
+      <Header></Header>
       <nav>
         {/* html, css, js와 같은 구체적인 글을 보는 페이지로 이동하는 영역 */}
         <ol>
