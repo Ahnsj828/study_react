2de033b8c8727840d34401ea4a8fd4bb5aa421f3
Ahnsj828 <ala828@naver.com>
2024-07-31 00:55:49 +0900

    0731

--- lifecoding/src/index.js
+++ lifecoding/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App4 from "./App4";
 import App5 from "./App5";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
