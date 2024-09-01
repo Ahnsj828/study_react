2fec9615aadbfa0267f6cd46800f4161e4c072a3
Ahnsj828 <ala828@naver.com>
2024-09-01 14:41:20 +0900

    user computer 결과 출력

--- coalnu/rockpaperscissors/src/components/Box.js
+++ coalnu/rockpaperscissors/src/components/Box.js
@@ -1,13 +1,26 @@
 import React from "react";
-import rock from "../assets/imgs/rock.png";
 
 const Box = (props) => {
   // console.log("props:", props);
+  let result;
+  if (
+    props.title === "Computer" &&
+    props.result !== "tie" &&
+    props.result !== ""
+  ) {
+    // 카드가 computer카드인가? && 결과가 비긴건 아닌가? && props.result에 값이 있는가?
+    result = props.result === "Win" ? "Lose" : "Win";
+  } else {
+    // 위의 경우가 아니라면 props&nbsb;로 전달된 결과를 그래도 쓴다.
+    result = props.result;
+  }
+
   return (
     <div className="box">
       <h1>{props.title}</h1>
+      <h2>{props.item && props.item.name}</h2>
       <img className="item-img" src={props.item && props.item.img} />
-      <h2>{props.result}</h2>
+      <h2>{result}</h2>
     </div>
   );
 };
