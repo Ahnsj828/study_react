8b02d4065928d0546e75e3b19da3df0b3460f9c2
Ahnsj828 <ala828@naver.com>
2025-01-27 16:28:39 +0900

    현재위치 기반 날씨 가져오기

--- coalnu/weather-api01/src/App.js
+++ coalnu/weather-api01/src/App.js
@@ -20,8 +20,11 @@ function App() {
     });
   };
 
-  const getWeatherByCurrentLocation = (lat, lon) => {
+  const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
   };
 
   useEffect(() => {
