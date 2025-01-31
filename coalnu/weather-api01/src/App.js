e84ac5c8973eeb3c9f976035471eca39478d475b
Ahnsj828 <ala828@naver.com>
2025-01-31 15:27:00 +0900

    weather data send to WeatherBox

--- coalnu/weather-api01/src/App.js
+++ coalnu/weather-api01/src/App.js
@@ -40,7 +40,7 @@ function App() {
   return (
     // <div>
     <div className="container">
-      <WeatherBox />
+      <WeatherBox weather={weather} />
       <WeatherButton />
     </div>
     // </div>
