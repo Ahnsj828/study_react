8ff45e4fe7469299f82c6ad1e9bdb0c85758f785
Ahnsj828 <ala828@naver.com>
2025-06-18 16:52:40 +0900

    practice react

---
+++ practice/my-app/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+      <main>
+        <p>Feel free to explore and modify the code!</p>
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2023 My React App</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
